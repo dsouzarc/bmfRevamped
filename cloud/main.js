7f558c47fdfbf90c49669932eceebc8138d00afe
Ryan D'souza <dsouzarc@gmail.com>
2015-02-26 18:15:46 -0500

    Working logic to add a new restaurant with an array of menu items

--- cloud/main.js
+++ cloud/main.js
@@ -5,6 +5,37 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello Ryan!");
 });
 
+Parse.Cloud.define("addRestaurant", function(request, response) {
+
+    var RestaurantObject = Parse.Object.extend("Restaurant");
+    var newRestaurant = new RestaurantObject();
+
+    var MenuItem = Parse.Object.extend("RestaurantItem");
+    var menuArray = [];
+
+    var menu = request.params.restaurantMenu.menu;
+    var restaurantName = request.params.restaurantName;
+
+    for(var i = 0; i < menu.length; i++) {
+        var item = new MenuItem();
+        item.save({restaurantOwner:restaurantName, itemName:menu[i].name, itemCost:menu[i].price, itemDescription:menu[i].description});
+        menuArray.push(item);
+    }
+
+
+    newRestaurant.save( {
+        restaurantName: request.params.restaurantName,
+        restaurantMenu: menuArray//menu
+        }, {
+        success: function(item) {
+            response.success();
+            console.log("Successful save");
+        },
+        error: function(item, error) {
+            response.error(error);
+        }
+        });
+});
 
 Parse.Cloud.define("addRestaurantItem", function(request, response) {
 
